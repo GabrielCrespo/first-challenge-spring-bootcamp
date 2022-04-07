b5f1ea82bfcc169f4b2a2ee613ad3b16436f15b2
GabrielCrespo <gabriel.crespo1942@gmail.com>
2022-04-07 00:50:02 -0300

    Start project, client entity

---
+++ backend/src/main/java/com/gabriel/firstchallenge/BootcampspringfirstchallengeApplication.java
@@ -0,0 +1,13 @@
+package com.gabriel.firstchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootcampspringfirstchallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootcampspringfirstchallengeApplication.class, args);
+	}
+
+}
